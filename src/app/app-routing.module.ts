9d18f600591174a49bed4f17235025be865f2857
Jefersson Bustamante <jeferb93@gmail.com>
2021-02-03 21:50:32 -0500

    added basic settings

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/index/index.module').then((m) => m.IndexModule),
+  },
+  {
+    path: 'news',
+    loadChildren: () =>
+      import('./pages/news/news.module').then((m) => m.NewsModule),
+  },
+  {
+    path: 'not-found',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+  { path: '**', redirectTo: 'not-found' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/components/alert-information/alert-information.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertInformationComponent } from './alert-information.component';
+
+
+
+@NgModule({
+  declarations: [AlertInformationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AlertInformationModule { }
